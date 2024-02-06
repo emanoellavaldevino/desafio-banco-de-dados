82c2541f0643145759718f94387315bc2d3325a8
Emanoella <emanoella.almeida@gmail.com>
2024-02-06 16:52:26 -0300

    adicionando o desafio no github

---
+++ consulta.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+# Conectar ao banco de dados ou criar um novo
+conexao = sqlite3.connect('banco') #conect ao arquivo biblioteca
+
+cursor = conexao.cursor()
+
+# - Listar todos os livros disponíveis
+# dados = cursor.execute('SELECT * FROM livros')
+# for livros in dados:
+#     print(livros)
+
+# Encontrar todos os livros emprestados no momento
+
+dados = cursor.execute('SELECT Estado_do_Exemplar FROM Emprestimos WHERE Estado_do_Exemplar = "emprestado"')
+print(dados)
+
+# Localizar os livros escritos por um autor específico
+# cursor.execute('SELECT * FROM Livros WHERE autor= "nome do autor"')
+
+# Verificar o número de cópias disponíveis de um determinado livro
+# cursor.execute('SELECT Exemplares_disponiveis FROM Livros WHERE titulo="nome do livro"')
+
+# Mostrar os empréstimos em atraso.
+# cursor.execute('SELECT livros.título, usuários.nome,empréstimos.data_de_empréstimo, empréstimos.data_de_devolução,\
+#                 FROM emprestimos\
+#                 INNER JOIN empréstimos ON livros.id_do_livro = empréstimos.id_do_livro')
+# delete remover um autor
+# cursor.execute('DELETE  FROM Livros WHERE autor ="Paulo Coelho"')
+# remover um livro
+
+# cursor.execute('DELETE FROM Livros WHERE titulo="SENHOR DOS ANEIS"')
+# marcar um livro como devolvido
+
+# cursor.execute('UPDATE Livros SET Estado_do_Exemplar = "devolvido" WHERE titulo="SENHOR DOS ANEIS"')
+
+# cursor.execute('UPDATE Livros SET Exemplares_disponiveis = Exemplares_disponiveis - 1 WHERE titulo="SENHOR DOS ANEIS"')
+
+conexao.commit()
+conexao.close()
